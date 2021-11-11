f40b7532533ac33d8a669a72ddd5e50074868ee0
hdseventh <hdseventh.hq@gmail.com>
2021-11-12 02:18:03 +0700

    Add project files.

---
+++ OfflinePlayer Ban/Main.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Newtonsoft.Json;
+using TShockAPI.DB;
+
+namespace OfflinePlayer_Ban
+{
+	[ApiVersion(2, 1)]
+	public class OfflinePlayer_Ban : TerrariaPlugin
+	{
+		#region Plugin Info
+		public override string Name => "OfflinePlayer Ban";
+		public override string Author => "hdseventh";
+		public override string Description => "A plugin to ban Offline players.";
+		public override Version Version { get { return new Version(1, 0, 0, 0); } }
+		#endregion
+
+		public OfflinePlayer_Ban(Main game) : base(game)
+		{
+
+		}
+
+		#region Initialize/Dispose
+		public override void Initialize()
+		{
+			ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+			}
+			base.Dispose(disposing);
+		}
+		#endregion
+
+
+		#region Hooks
+		private void OnInitialize(EventArgs args)
+		{
+			Commands.ChatCommands.Add(new Command("opb.ban", OfflineBan, "oban"));
+			Commands.ChatCommands.Add(new Command("opb.ban", IPBan, "ipban"));
+		}
+		#endregion
+
+		#region Commands
+		private void OfflineBan(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendErrorMessage($"Invalid syntax: {TShock.Config.Settings.CommandSpecifier}oban <username> <d/m/h/s>");
+				return;
+			}
+
+			var user = TShock.UserAccounts.GetUserAccountByName(args.Parameters[0]);
+			if (user == null)
+			{
+				args.Player.SendErrorMessage("Unknown account.");
+				return;
+			}
+			var iplist = JsonConvert.DeserializeObject<List<string>>(user.KnownIps);
+			if (iplist.Count == 0 || string.IsNullOrWhiteSpace(user.UUID))
+			{
+				args.Player.SendErrorMessage("This is an empty account.");
+				return;
+			}
+
+			AddBanResult banResult;
+			string reason = "Banned.";
+			string duration = null;
+			var result = new System.Text.StringBuilder();
+			DateTime expiration = DateTime.MaxValue;
+
+			if(args.Parameters.Count > 1)
+            {
+				duration = args.Parameters[1];
+				if (TShockAPI.TShock.Utils.TryParseTime(duration, out int seconds))
+				{
+					expiration = DateTime.UtcNow.AddSeconds(seconds);
+				}
+			}
+			string[] identifier = { $"acc:{user.Name}", $"uuid:{user.UUID}"};
+			for (int i = 0; i <= 1; i++)
+			{
+				banResult = TShockAPI.TShock.Bans.InsertBan(identifier[i], reason, reason, DateTime.UtcNow, expiration);
+				if (banResult.Ban != null)
+				{
+					result.Append($"Ban added. Ticket Number {banResult.Ban.TicketNumber} was created for identifier {identifier[i]}.\n");
+				}
+				else
+				{
+					result.Append($"Failed to add ban for identifier: {identifier[i]}\n");
+					result.Append($"Reason: {banResult.Message}\n");
+				}
+
+			}
+
+			foreach(var ip in iplist)
+            {
+				banResult = TShockAPI.TShock.Bans.InsertBan($"ip:{ip}", reason, reason, DateTime.UtcNow, expiration);
+				if (banResult.Ban != null)
+				{
+					result.Append($"Ban added. Ticket Number {banResult.Ban.TicketNumber} was created for identifier ip:{ip}.\n");
+				}
+				else
+				{
+					result.Append($"Failed to add ban for identifier: ip:{ip}\n");
+					result.Append($"Reason: {banResult.Message}\n");
+				}
+			}
+			args.Player.SendSuccessMessage(result.ToString());
+		}
+
+		private void IPBan(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendErrorMessage($"Invalid syntax: {TShock.Config.Settings.CommandSpecifier}ipban <username> <d/m/h/s>");
+				return;
+			}
+
+			var user = TShock.UserAccounts.GetUserAccountByName(args.Parameters[0]);
+			if (user == null)
+			{
+				args.Player.SendErrorMessage("Unknown account.");
+				return;
+			}
+			var iplist = JsonConvert.DeserializeObject<List<string>>(user.KnownIps);
+			if (iplist.Count == 0 || string.IsNullOrWhiteSpace(user.UUID))
+			{
+				args.Player.SendErrorMessage("This is an empty account.");
+				return;
+			}
+
+			AddBanResult banResult;
+			string reason = "Banned.";
+			string duration = null;
+			var result = new System.Text.StringBuilder();
+			DateTime expiration = DateTime.MaxValue;
+
+			if (args.Parameters.Count > 1)
+			{
+				duration = args.Parameters[1];
+				if (TShockAPI.TShock.Utils.TryParseTime(duration, out int seconds))
+				{
+					expiration = DateTime.UtcNow.AddSeconds(seconds);
+				}
+			}
+
+			foreach (var ip in iplist)
+			{
+				banResult = TShockAPI.TShock.Bans.InsertBan($"ip:{ip}", reason, reason, DateTime.UtcNow, expiration);
+				if (banResult.Ban != null)
+				{
+					result.Append($"Ban added. Ticket Number {banResult.Ban.TicketNumber} was created for identifier ip:{ip}.\n");
+				}
+				else
+				{
+					result.Append($"Failed to add ban for identifier: ip:{ip}\n");
+					result.Append($"Reason: {banResult.Message}\n");
+				}
+			}
+			args.Player.SendSuccessMessage(result.ToString());
+		}
+		#endregion
+	}
+}
